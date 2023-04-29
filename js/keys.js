db8032f5b24ddf866ff3556b8178df105a4b8a32
Western8 <westernchrome@gmail.com>
2023-04-30 02:04:54 +0300

    feat: add lib keys

---
+++ js/keys.js
@@ -0,0 +1,200 @@
+export default [
+  {
+    code: 'KeyQ', keyEnUp: 'Q', keyEnLow: 'q', keyRuUp: 'Й', keyRuLow: 'й',
+  },
+  {
+    code: 'KeyW', keyEnUp: 'W', keyEnLow: 'w', keyRuUp: 'Ц', keyRuLow: 'ц',
+  },
+  {
+    code: 'KeyE', keyEnUp: 'E', keyEnLow: 'e', keyRuUp: 'У', keyRuLow: 'у',
+  },
+  {
+    code: 'KeyR', keyEnUp: 'R', keyEnLow: 'r', keyRuUp: 'К', keyRuLow: 'к',
+  },
+  {
+    code: 'KeyT', keyEnUp: 'T', keyEnLow: 't', keyRuUp: 'Е', keyRuLow: 'е',
+  },
+  {
+    code: 'KeyY', keyEnUp: 'Y', keyEnLow: 'y', keyRuUp: 'Н', keyRuLow: 'н',
+  },
+  {
+    code: 'KeyU', keyEnUp: 'U', keyEnLow: 'u', keyRuUp: 'Г', keyRuLow: 'г',
+  },
+  {
+    code: 'KeyI', keyEnUp: 'I', keyEnLow: 'i', keyRuUp: 'Ш', keyRuLow: 'ш',
+  },
+  {
+    code: 'KeyO', keyEnUp: 'O', keyEnLow: 'o', keyRuUp: 'Щ', keyRuLow: 'щ',
+  },
+  {
+    code: 'KeyP', keyEnUp: 'P', keyEnLow: 'p', keyRuUp: 'З', keyRuLow: 'з',
+  },
+  {
+    code: 'KeyA', keyEnUp: 'A', keyEnLow: 'a', keyRuUp: 'Ф', keyRuLow: 'ф',
+  },
+  {
+    code: 'KeyS', keyEnUp: 'S', keyEnLow: 's', keyRuUp: 'Ы', keyRuLow: 'ы',
+  },
+  {
+    code: 'KeyD', keyEnUp: 'D', keyEnLow: 'd', keyRuUp: 'В', keyRuLow: 'в',
+  },
+  {
+    code: 'KeyF', keyEnUp: 'F', keyEnLow: 'f', keyRuUp: 'А', keyRuLow: 'а',
+  },
+  {
+    code: 'KeyG', keyEnUp: 'G', keyEnLow: 'g', keyRuUp: 'П', keyRuLow: 'п',
+  },
+  {
+    code: 'KeyH', keyEnUp: 'H', keyEnLow: 'h', keyRuUp: 'Р', keyRuLow: 'р',
+  },
+  {
+    code: 'KeyJ', keyEnUp: 'J', keyEnLow: 'j', keyRuUp: 'О', keyRuLow: 'о',
+  },
+  {
+    code: 'KeyK', keyEnUp: 'K', keyEnLow: 'k', keyRuUp: 'Л', keyRuLow: 'л',
+  },
+  {
+    code: 'KeyL', keyEnUp: 'L', keyEnLow: 'l', keyRuUp: 'Д', keyRuLow: 'д',
+  },
+  {
+    code: 'KeyZ', keyEnUp: 'Z', keyEnLow: 'z', keyRuUp: 'Я', keyRuLow: 'я',
+  },
+  {
+    code: 'KeyX', keyEnUp: 'X', keyEnLow: 'x', keyRuUp: 'Ч', keyRuLow: 'ч',
+  },
+  {
+    code: 'KeyC', keyEnUp: 'C', keyEnLow: 'c', keyRuUp: 'С', keyRuLow: 'с',
+  },
+  {
+    code: 'KeyV', keyEnUp: 'V', keyEnLow: 'v', keyRuUp: 'М', keyRuLow: 'м',
+  },
+  {
+    code: 'KeyB', keyEnUp: 'B', keyEnLow: 'b', keyRuUp: 'И', keyRuLow: 'и',
+  },
+  {
+    code: 'KeyN', keyEnUp: 'N', keyEnLow: 'n', keyRuUp: 'Т', keyRuLow: 'т',
+  },
+  {
+    code: 'KeyM', keyEnUp: 'M', keyEnLow: 'm', keyRuUp: 'Ь', keyRuLow: 'ь',
+  },
+
+  {
+    code: 'Digital1', keyEnUp: '!', keyEnLow: '1', keyRuUp: '!', keyRuLow: '1',
+  },
+  {
+    code: 'Digital2', keyEnUp: '@', keyEnLow: '2', keyRuUp: '"', keyRuLow: '2',
+  },
+  {
+    code: 'Digital3', keyEnUp: '#', keyEnLow: '3', keyRuUp: '№', keyRuLow: '3',
+  },
+  {
+    code: 'Digital4', keyEnUp: '$', keyEnLow: '4', keyRuUp: ';', keyRuLow: '4',
+  },
+  {
+    code: 'Digital5', keyEnUp: '%', keyEnLow: '5', keyRuUp: '%', keyRuLow: '5',
+  },
+  {
+    code: 'Digital6', keyEnUp: '^', keyEnLow: '6', keyRuUp: ':', keyRuLow: '6',
+  },
+  {
+    code: 'Digital7', keyEnUp: '&', keyEnLow: '7', keyRuUp: '?', keyRuLow: '7',
+  },
+  {
+    code: 'Digital8', keyEnUp: '*', keyEnLow: '8', keyRuUp: '*', keyRuLow: '8',
+  },
+  {
+    code: 'Digital9', keyEnUp: '(', keyEnLow: '9', keyRuUp: '(', keyRuLow: '9',
+  },
+  {
+    code: 'Digital0', keyEnUp: ')', keyEnLow: '0', keyRuUp: ')', keyRuLow: '0',
+  },
+
+  {
+    code: 'Backquote', keyEnUp: '~', keyEnLow: '`', keyRuUp: 'Ё', keyRuLow: 'ё',
+  },
+  {
+    code: 'Minus', keyEnUp: '_', keyEnLow: '-', keyRuUp: '_', keyRuLow: '-',
+  },
+  {
+    code: 'Equal', keyEnUp: '+', keyEnLow: '=', keyRuUp: '+', keyRuLow: '=',
+  },
+  {
+    code: 'BracketLeft', keyEnUp: '{', keyEnLow: '[', keyRuUp: 'Х', keyRuLow: 'х',
+  },
+  {
+    code: 'BracketRight', keyEnUp: '}', keyEnLow: ']', keyRuUp: 'Ъ', keyRuLow: 'ъ',
+  },
+  {
+    code: 'Semicolon', keyEnUp: ':', keyEnLow: ';', keyRuUp: 'Ж', keyRuLow: 'ж',
+  },
+  {
+    code: 'Quote', keyEnUp: '"', keyEnLow: "'", keyRuUp: 'Э', keyRuLow: 'э',
+  },
+  {
+    code: 'Backslash', keyEnUp: '|', keyEnLow: '\\', keyRuUp: '/', keyRuLow: '\\',
+  },
+  {
+    code: 'Comma', keyEnUp: '<', keyEnLow: ',', keyRuUp: 'Б', keyRuLow: 'б',
+  },
+  {
+    code: 'Period', keyEnUp: '>', keyEnLow: '.', keyRuUp: 'Ю', keyRuLow: 'ю',
+  },
+  {
+    code: 'Slash', keyEnUp: '?', keyEnLow: '/', keyRuUp: ',', keyRuLow: '.',
+  },
+
+  {
+    code: 'ArrowUp', keyEnUp: '↑', keyEnLow: '↑', keyRuUp: '↑', keyRuLow: '↑',
+  },
+  {
+    code: 'ArrowDown', keyEnUp: '↓', keyEnLow: '↓', keyRuUp: '↓', keyRuLow: '↓',
+  },
+  {
+    code: 'ArrowLeft', keyEnUp: '←', keyEnLow: '←', keyRuUp: '←', keyRuLow: '←',
+  },
+  {
+    code: 'ArrowRight', keyEnUp: '→', keyEnLow: '→', keyRuUp: '→', keyRuLow: '→',
+  },
+  {
+    code: 'Backspace', keyEnUp: 'Backspace', keyEnLow: 'Backspace', keyRuUp: 'Backspace', keyRuLow: 'Backspace',
+  },
+  {
+    code: 'Tab', keyEnUp: 'Tab', keyEnLow: 'Tab', keyRuUp: 'Tab', keyRuLow: 'Tab',
+  },
+  {
+    code: 'Delete', keyEnUp: 'Del', keyEnLow: '', keyRuUp: '', keyRuLow: '',
+  },
+  {
+    code: 'CapsLock', keyEnUp: 'CapsLock', keyEnLow: 'CapsLock', keyRuUp: 'CapsLock', keyRuLow: 'CapsLock',
+  },
+  {
+    code: 'Enter', keyEnUp: 'Enter', keyEnLow: '', keyRuUp: '', keyRuLow: '',
+  },
+  {
+    code: 'ShiftLeft', keyEnUp: 'Shift', keyEnLow: 'Shift', keyRuUp: 'Shift', keyRuLow: 'Shift',
+  },
+  {
+    code: 'ShiftRight', keyEnUp: 'Shift', keyEnLow: 'Shift', keyRuUp: 'Shift', keyRuLow: 'Shift',
+  },
+  {
+    code: 'ControlLeft', keyEnUp: 'Ctrl', keyEnLow: 'Ctrl', keyRuUp: 'Ctrl', keyRuLow: 'Ctrl',
+  },
+  {
+    code: 'ControlRight', keyEnUp: 'Ctrl', keyEnLow: 'Ctrl', keyRuUp: 'Ctrl', keyRuLow: 'Ctrl',
+  },
+  {
+    code: 'AltLeft', keyEnUp: 'Alt', keyEnLow: 'Alt', keyRuUp: 'Alt', keyRuLow: 'Alt',
+  },
+  {
+    code: 'AltRight', keyEnUp: 'Alt', keyEnLow: 'Alt', keyRuUp: 'Alt', keyRuLow: 'Alt',
+  },
+  {
+    code: 'MetaLeft', keyEnUp: 'Win', keyEnLow: '', keyRuUp: '', keyRuLow: '',
+  },
+  {
+    code: 'MetaRight', keyEnUp: 'Win', keyEnLow: '', keyRuUp: '', keyRuLow: '',
+  },
+  {
+    code: 'Space', keyEnUp: ' ', keyEnLow: '', keyRuUp: '', keyRuLow: '',
+  },
+];
--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+import keys from './js/keys.js';
+
 const body = document.querySelector('body');
-let lang = 'en';
-const keys = [
-  { keyEn: 'q', keyRu: 'й', code: 'KeyQ' },
-  { keyEn: 'w', keyRu: 'ц', code: 'KeyW' },
-  { keyEn: 'e', keyRu: 'у', code: 'KeyE' },
-  { keyEn: 'r', keyRu: 'к', code: 'KeyR' },
-  { keyEn: 'T', keyRu: 'е', code: 'KeyT' },
-  { keyEn: 'Y', keyRu: 'н', code: 'KeyY' },
-  { keyEn: 'U', keyRu: 'г', code: 'KeyU' },
-];
+const state = {
+  lang: 'en',
+  shift: false,
+  capsLock: false,
+  up: false,
+};
+
+const keysRow1 = ['Backquote', 'Digital1', 'Digital2', 'Digital3', 'Digital4', 'Digital5', 'Digital6', 'Digital7', 'Digital8', 'Digital9', 'Digital0', 'Minus', 'Equal', 'Backspace'];
+const keysRow2 = ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete'];
+const keysRow3 = ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'];
+const keysRow4 = ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'];
+const keysRow5 = ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
+const keysRows = [...keysRow1, ...keysRow2, ...keysRow3, ...keysRow4, ...keysRow5];
+const keysMiddle = ['Tab', 'ControlLeft', 'ControlRight', 'ShiftRight'];
+const keysBig = ['Backspace', 'CapsLock', 'Enter', 'ShiftLeft'];
 
 const wrapper = document.createElement('div');
 wrapper.classList.add('wrapper');
@@ -26,11 +33,50 @@ function addText(text) {
   input.setRangeText(text, input.selectionStart, input.selectionEnd, 'end');
 }
 
-function pressKey(e) {
-  // e.dataset.code;
-  const text = e.target.dataset.keyEn;
+/*
+function clickKey(e) {
+  let code = e.target.dataset.code;
+  if (code === 'CapsLock') {
+    state.capsLock = !state.capsLock;
+  };
+  state.up = (state.shift && !state.capsLock) || (state.capsLock && !state.shift);
+
+  let text = '';
+  if (state.lang === 'ru') {
+    text = (state.up) ? e.target.dataset.keyRuUp : e.target.dataset.keyRuLow;
+  } else {
+    text = (state.up) ? e.target.dataset.keyEnUp : e.target.dataset.keyEnLow;
+  }
+  //const text = e.target.dataset.keyEnUp;
   // input.textContent += symb;
   // let textContent = input.textContent;
+  addText(text);
+}
+*/
+
+function mousedownKey(e) {
+  const { code } = e.target.dataset;
+  if (code === 'ShiftLeft' || code === 'ShiftRight') {
+    state.shift = true;
+  }
+}
+
+function mouseupKey(e) {
+  const { code } = e.target.dataset;
+  if (code === 'ShiftLeft' || code === 'ShiftRight') {
+    state.shift = false;
+  }
+  if (code === 'CapsLock') {
+    state.capsLock = !state.capsLock;
+  }
+  state.up = (state.shift && !state.capsLock) || (state.capsLock && !state.shift);
+
+  let text = '';
+  if (state.lang === 'ru') {
+    text = (state.up) ? e.target.dataset.keyRuUp : e.target.dataset.keyRuLow;
+  } else {
+    text = (state.up) ? e.target.dataset.keyEnUp : e.target.dataset.keyEnLow;
+  }
   addText(text);
 }
 
@@ -45,7 +91,7 @@ function pressKeyup(e) {
   const key = document.getElementById(e.code);
   if (key) {
     key.classList.remove('active');
-    addText(key.dataset.keyEn);
+    addText(key.dataset.keyEnUp);
   }
 }
 
@@ -59,26 +105,60 @@ function createRow() {
 function createKey(row, objKey) {
   const key = document.createElement('div');
   key.classList.add('key');
+  if (keysMiddle.includes(objKey.code)) {
+    key.classList.add('size-middle');
+  } else if (keysBig.includes(objKey.code)) {
+    key.classList.add('size-big');
+  }
   key.id = objKey.code;
   key.dataset.code = objKey.code;
-  key.dataset.keyEn = objKey.keyEn;
-  key.textContent = objKey.keyEn;
-  key.addEventListener('click', pressKey);
+  key.dataset.keyEnUp = objKey.keyEnUp;
+  key.dataset.keyEnLow = objKey.keyEnLow;
+  key.dataset.keyRuUp = objKey.keyRuUp;
+  key.dataset.keyRuLow = objKey.keyRuLow;
+  key.textContent = objKey.keyEnLow;
+  // key.addEventListener('click', clickKey);
+  key.addEventListener('mousedown', mousedownKey);
+  key.addEventListener('mouseup', mouseupKey);
   row.append(key);
 }
 
-let row = createRow();
-
-keys.forEach((item) => {
-  createKey(row, item);
+keysRows.forEach((item) => {
+  const keyItem = keys.find((k) => (k.code === item));
+  createKey(keyboard, keyItem);
 });
+
+/*
+let row = createRow();
+keysRow1.forEach((item) => {
+  const keyItem = keys.find((k) => (k.code === item));
+  createKey(row, keyItem);
+});
+
+row = createRow();
+keysRow2.forEach((item) => {
+  const keyItem = keys.find((k) => (k.code === item));
+  createKey(row, keyItem);
+});
+
+row = createRow();
+keysRow3.forEach((item) => {
+  const keyItem = keys.find((k) => (k.code === item));
+  createKey(row, keyItem);
+});
+
+row = createRow();
+keysRow4.forEach((item) => {
+  const keyItem = keys.find((k) => (k.code === item));
+  createKey(row, keyItem);
+});
+
+row = createRow();
+keysRow5.forEach((item) => {
+  const keyItem = keys.find((k) => (k.code === item));
+  createKey(row, keyItem);
+});
+*/
 
 document.addEventListener('keydown', pressKeydown);
 document.addEventListener('keyup', pressKeyup);
-
-/*
-function getPos() {
-  let pos = input.selectionStart;
-  return pos;
-}
-*/
